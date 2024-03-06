8ecd15515d37017db784528f0b96b4d5e3e77cc7
Santiago Posada <santiago90dell@gmail.com>
2024-03-06 14:10:06 -0500

    ready to present the archetype model

--- apps/clients-app/src/clients-app.module.ts
+++ apps/clients-app/src/clients-app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ClientsAppService } from './clients-app.service';
 import { ClientController } from 'infrastructure/entry-points/client/client.controller';
 import { ClientServiceMongo } from 'infrastructure/driven-adapters/client/client.mongo';
 import { CreateClientUseCase } from 'domain/use-case/client/create-client.use-case';
-import { ClientRepository } from 'domain/model/client/gateway/client.repository';
 
 @Module({
   imports: [],
   controllers: [ClientController],
   providers: [
+    ClientServiceMongo,
     {
       provide: CreateClientUseCase,
       useFactory: (clientServiceMongo: ClientServiceMongo) =>
--- infrastructure/driven-adapters/client/client.mongo.ts
+++ infrastructure/driven-adapters/client/client.mongo.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { Client } from "domain/model/client/client.model";
 import { ClientRepository } from "domain/model/client/gateway/client.repository";
-import { Observable, from } from "rxjs";
+import { Observable, of } from "rxjs";
 
 @Injectable()
 export class ClientServiceMongo implements ClientRepository {
     saveClient(client: Client): Observable<Client> {
         console.log('Intentando crear client');
 
-        return from(client);
+        return of(client);
         
     }
     getAllClients(): Observable<Client> {
